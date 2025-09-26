2cc015bc5d873698152a90b5aefa66d682de0bd3
Moaz Elkholy <136008714+Mo3az1x@users.noreply.github.com>
2025-09-26 21:56:32 +0300

    Update gpio.c

--- Nucleo 2/Nucleo 2/Core/Src/gpio.c
+++ Nucleo 2/Nucleo 2/Core/Src/gpio.c
@@ -54,10 +54,19 @@ void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(SPI_CS_GPIO_Port, SPI_CS_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8|GPIO_PIN_9, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8|save_led_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(ERROR_LED_GPIO_Port, ERROR_LED_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(IDLE_LED_GPIO_Port, IDLE_LED_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(MOVING_LED_GPIO_Port, MOVING_LED_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pins : switch_mode_Pin Height_dwn_Pin Slide_dwn_Pin */
   GPIO_InitStruct.Pin = switch_mode_Pin|Height_dwn_Pin|Slide_dwn_Pin;
@@ -65,10 +74,12 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PA0 PA1 Height_up_Pin Slide_up_Pin
-                           Incline_up_Pin */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|Height_up_Pin|Slide_up_Pin
-                          |Incline_up_Pin;
+  /*Configure GPIO pins : reset_Pin PA1 Height_up_Pin Slide_up_Pin
+                           Incline_up_Pin PA8 PA9 BUTTON_LOAD_Pin
+                           BUTTON_SAVE_Pin */
+  GPIO_InitStruct.Pin = reset_Pin|GPIO_PIN_1|Height_up_Pin|Slide_up_Pin
+                          |Incline_up_Pin|GPIO_PIN_8|GPIO_PIN_9|BUTTON_LOAD_Pin
+                          |BUTTON_SAVE_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
@@ -86,22 +97,86 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(incline_dwn_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PB12 */
-  GPIO_InitStruct.Pin = GPIO_PIN_12;
+  /*Configure GPIO pin : SPI_CS_Pin */
+  GPIO_InitStruct.Pin = SPI_CS_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  HAL_GPIO_Init(SPI_CS_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PC8 PC9 */
-  GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9;
+  /*Configure GPIO pins : PC8 save_led_Pin */
+  GPIO_InitStruct.Pin = GPIO_PIN_8|save_led_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
+  /*Configure GPIO pin : ERROR_LED_Pin */
+  GPIO_InitStruct.Pin = ERROR_LED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(ERROR_LED_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : IDLE_LED_Pin */
+  GPIO_InitStruct.Pin = IDLE_LED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(IDLE_LED_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : MOVING_LED_Pin */
+  GPIO_InitStruct.Pin = MOVING_LED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(MOVING_LED_GPIO_Port, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 2 */
+void ErrorLED_On(void)
+{
+    HAL_GPIO_WritePin(ERROR_LED_GPIO_Port, ERROR_LED_Pin, GPIO_PIN_SET);
+}
 
+void ErrorLED_Off(void)
+{
+    HAL_GPIO_WritePin(ERROR_LED_GPIO_Port, ERROR_LED_Pin, GPIO_PIN_RESET);
+}
+
+void ErrorLED_Toggle(void)
+{
+    HAL_GPIO_TogglePin(ERROR_LED_GPIO_Port, ERROR_LED_Pin);
+}
+
+void IdleLED_On(void)
+{
+    HAL_GPIO_WritePin(IDLE_LED_GPIO_Port, IDLE_LED_Pin, GPIO_PIN_SET);
+}
+
+void IdleLED_Off(void)
+{
+    HAL_GPIO_WritePin(IDLE_LED_GPIO_Port, IDLE_LED_Pin, GPIO_PIN_RESET);
+}
+
+void IdleLED_Toggle(void)
+{
+    HAL_GPIO_TogglePin(IDLE_LED_GPIO_Port, IDLE_LED_Pin);
+}
+
+void MovingLED_On(void)
+{
+    HAL_GPIO_WritePin(MOVING_LED_GPIO_Port, MOVING_LED_Pin, GPIO_PIN_SET);
+}
+
+void MovingLED_Off(void)
+{
+    HAL_GPIO_WritePin(MOVING_LED_GPIO_Port, MOVING_LED_Pin, GPIO_PIN_RESET);
+}
+
+void MovingLED_Toggle(void)
+{
+    HAL_GPIO_TogglePin(MOVING_LED_GPIO_Port, MOVING_LED_Pin);
+}
 /* USER CODE END 2 */
